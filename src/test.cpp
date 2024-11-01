5194276bb10cc33e223c724a156fcf45e29e9184
23310392 <a23310392@ceti.mx>
2024-11-01 01:40:14 +0000

    Clase 4

--- src/test.cpp
+++ src/test.cpp
@@ -12,17 +12,23 @@ int main(int argc, char const *argv[])
 {
     std::string reset_position;
     int frame = 0;
-    auto can = Canvas(300,200);
+    auto can = Canvas(300,250);
     can.DrawPointCircle(170,40,28);
     can.DrawPointCircle(35,40,10);
+    
 
     while (true)
     {
-        auto can = Canvas(200,200);
+        auto can = Canvas(300,300);
         can.DrawPointCircle(170,40,28);
         can.DrawPointCircle(frame,40,10);
+        can.DrawBlockEllipse(170,40,50,4);
+        if(frame>=140){
+            can.DrawBlockCircleFilled(142,40,g++);
+        }
         if(frame==140){
-            can.DrawBlockCircleFilled(120,40,g++);
+
+            frame--;
         }
         Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full());
         Element personaje = spinner(21, frame);
@@ -31,7 +37,7 @@ int main(int argc, char const *argv[])
         std::cout << reset_position;
         pantalla.Print();
         reset_position = pantalla.ResetPosition(true);
-        this_thread::sleep_for(0.05s);
+        this_thread::sleep_for(0.035s);
         frame++;
     }
 
